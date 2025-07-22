295a65ae24d2822f72c7de1e088c6be640360e12
Nakyanzi Catherine <n84252928@china.huawei.com>
2025-07-22 13:02:16 +0300

    added git repo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
       <h1>
         Weather App
       </h1>
+
+      <footer>
+        This project was coded by <a href='https://ug.linkedin.com/in/nakyanzi-catheriine-901b031a3'>Cathy</a> and is open-source on 
+        <a href='https://github.com/catherine256/week5'>
+           <span> open-sourced on GitHub</span>
+        </a>
+      </footer>
     </div>
   );
 }
