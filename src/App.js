5677de582f1c63a696957290ea64c99ae082b168
Nakyanzi Catherine <n84252928@china.huawei.com>
2025-07-22 14:02:08 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Weather App
-      </h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by <a href='https://ug.linkedin.com/in/nakyanzi-catheriine-901b031a3'>Cathy</a> and is open-source on 
-        <a href='https://github.com/catherine256/week5'>
-           <span> open-sourced on GitHub</span>
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://ug.linkedin.com/in/nakyanzi-catheriine-901b031a3">
+            Cathy
+          </a>{" "}
+          and is open-source on{" "}
+          <a href="https://github.com/catherine256/week5">
+            <span>open-sourced on GitHub</span>
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
